b63d7d9715913a958536a71371d471515a03925a
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-09-28 13:14:11 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+    <header>
+      <h1>Soccer</h1>
+    </header>
+    <main>
+      <div className="field">
+        <div className="f1"></div>
+        <div className="f2"></div>
+
+        <div className="f30"></div>
+        <div className="f31"></div>
+        <div className="f32"></div>
+        <div className="f33"></div>
+
+        <div className="f40"></div>
+        <div className="f41"></div>
+        <div className="f42"></div>
+        <div className="f43"></div>
+      </div>
+    </main>
+    <footer>
+      Made by Tomasz Pięta
+    </footer>
+  </div>
+  );
+}
+export default App;
